a4fe19cd9d3d7dd47d1a54406e14b16edd222356
Nguyen Nghia <nghiaklpro195@gmail.com>
2020-04-15 15:17:41 +0700

    first commit

---
+++ StringHelper/String.swift
@@ -0,0 +1,19 @@
+//
+//  String.swift
+//  StringHelper
+//
+//  Created by Nguyen Van Nghia on 4/15/20.
+//  Copyright © 2020 Nguyen Van Nghia. All rights reserved.
+//
+
+import Foundation
+
+public extension String {
+    func toInt() -> Int? {
+        return Int(self)
+    }
+    
+    func toFloat() -> Float? {
+        return Float(self)
+    }
+}
---
+++ StringHelper/StringHelper.h
@@ -0,0 +1,19 @@
+//
+//  StringHelper.h
+//  StringHelper
+//
+//  Created by Nguyen Van Nghia on 4/15/20.
+//  Copyright © 2020 Nguyen Van Nghia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StringHelper.
+FOUNDATION_EXPORT double StringHelperVersionNumber;
+
+//! Project version string for StringHelper.
+FOUNDATION_EXPORT const unsigned char StringHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringHelper/PublicHeader.h>
+
+
